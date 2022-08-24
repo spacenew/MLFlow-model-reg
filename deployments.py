7a455aa96deb66fb5cb11c0907162ca0e7d4e4d8
ilija <68110781+spacenew@users.noreply.github.com>
2022-08-24 11:52:56 +0300

    Update deployments.py

--- deployments.py
+++ deployments.py
@@ -6,9 +6,10 @@ from datetime import timedelta
 
 DeploymentSpec(
     name="deploy-mlflow",
-    flow_name='mlflow-staging',
+    flow_name='main',
     schedule=IntervalSchedule(interval=timedelta(minutes=10)),
-    flow_location="./mlflow_main.py",
+    flow_location="./mlflow_main.py",  # main script
+    flow_storage='7a92c16b-eb0b-43a9-86b2-8d8104a9c761',  # local storage id
     flow_runner=SubprocessFlowRunner(),
     parameters={
         "train_data_path": './data/green_tripdata_2021-01.parquet',
